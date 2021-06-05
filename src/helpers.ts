dfc8e3ead35e795f394f821e3a934ed095233d2f
eilla1 <git@ella.cx>
2021-06-05 14:56:03 -0700

    fix: bug where the bricks show up in a single col

--- src/helpers.ts
+++ src/helpers.ts
@@ -14,7 +14,7 @@ import {
 export function createBricks(): Brick[] {
   return LEVEL.reduce((ack, element, i) => {
     const row = Math.floor((i + 1) / STAGE_COLS);
-    const col = 1 % STAGE_COLS;
+    const col = i % STAGE_COLS;
 
     const x = STAGE_PADDING + col * (BRICK_WIDTH + BRICK_PADDING);
     const y = STAGE_PADDING + row * (BRICK_HEIGHT + BRICK_PADDING);
