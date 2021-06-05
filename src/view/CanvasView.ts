1deea3c106d66cd49b8fe44e0b00fb480c2daf64
eilla1 <git@ella.cx>
2021-06-04 18:01:00 -0700

    :construction: scaffold index.ts with start view

--- src/view/CanvasView.ts
+++ src/view/CanvasView.ts
@@ -5,7 +5,7 @@ import { Ball } from "../sprites/Ball";
 import { BRICK_IMAGES } from "~/setup";
 
 export class CanvasView {
-  private canvas: HTMLCanvasElement;
+  canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D | null;
   private scoreDisplay: HTMLObjectElement | null;
   private start: HTMLObjectElement | null;
