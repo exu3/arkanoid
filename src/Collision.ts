b79e6ef780e7ecb283bae7ffeb984b5449976aff
eilla1 <git@ella.cx>
2021-06-05 01:25:27 -0700

    feat: add the checks for collision with bricks

--- src/Collision.ts
+++ src/Collision.ts
@@ -5,6 +5,38 @@ import { Ball } from "./sprites/Ball";
 import { CanvasView } from "./view/CanvasView";
 
 export class Collision {
+  isCollidingBrick(ball: Ball, brick: Brick): boolean {
+    if (
+      ball.pos.x < brick.pos.x + brick.width &&
+      ball.pos.x + ball.width > brick.pos.x &&
+      ball.pos.y < brick.pos.y + brick.height &&
+      ball.pos.y + ball.height > brick.pos.y
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  // check ball collision with bricks
+  isCollidingBricks(ball: Ball, bricks: Brick[]): boolean {
+    let colliding = false;
+
+    bricks.forEach((brick, i) => {
+      if (this.isCollidingBrick(ball, brick)) {
+        ball.changeYDirection();
+
+        if (brick.energy === 1) {
+          bricks.splice(i, 1);
+        } else {
+          brick.energy -= 1;
+        }
+        colliding = true;
+      }
+    });
+
+    return colliding;
+  }
+
   checkBallCollision(ball: Ball, paddle: Paddle, view: CanvasView): void {
     // check ball collision with paddle
     if (
@@ -20,7 +52,7 @@ export class Collision {
       ball.changeXDirection();
     }
     // ball movement y bounds
-    if (ball.pos.y < 0 + ball.height) {
+    if (ball.pos.y < 0) {
       ball.changeYDirection();
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -56,6 +56,13 @@ function gameLoop(
   }
 
   collision.checkBallCollision(ball, paddle, view);
+  const collidingBrick = collision.isCollidingBricks(ball, bricks);
+
+  if (collidingBrick) {
+    score += 1;
+    view.drawScore(score);
+  }
+
   requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision));
 }
 
